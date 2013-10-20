782eb5ebc6653e5cf61ba71469e5dabc021208c1
Ryan DV <rdevilla@uwaterloo.ca>
2013-10-20 03:28:42 -0400

    Convert hard tabs to soft

--- ai/introduction/saveprincess.hs
+++ ai/introduction/saveprincess.hs
@@ -16,27 +16,27 @@ select :: a -> [(Bool, a)] -> a
 select def = maybe def snd . find fst
 
 -- PRE: cell is an element of the grid.
-findCell			:: [String] -> Char -> Int -> (Int, Int)
-findCell (x:xs) cell acc	= case findIndex (== cell) x of
-  					Nothing -> findCell xs cell (1 + acc)
-					Just n  -> (acc, n)
+findCell                        :: [String] -> Char -> Int -> (Int, Int)
+findCell (x:xs) cell acc        = case findIndex (== cell) x of
+                                        Nothing -> findCell xs cell (1 + acc)
+                                        Just n  -> (acc, n)
 
-buildPath			:: Int -> (String, String) -> String
-buildPath delta (dir1, dir2)	= concat $ replicate (abs delta)
-     						(select "" [ ((delta > 0), dir1)
-					    	           , ((delta < 0), dir2) ])
+buildPath                       :: Int -> (String, String) -> String
+buildPath delta (dir1, dir2)    = concat $ replicate (abs delta)
+                                                (select "" [ ((delta > 0), dir1)
+                                                           , ((delta < 0), dir2) ])
 
 
 
-displayPathtoPrincess		:: Int -> [String] -> String
-displayPathtoPrincess _ grid	= let bot	= findCell grid 'm' 0
-				      princess  = findCell grid 'p' 0 in
-				      buildPath (fst bot - fst princess) ("UP\n", "DOWN\n")
-				   ++ buildPath (snd bot - snd princess) ("LEFT\n", "RIGHT\n")
+displayPathtoPrincess           :: Int -> [String] -> String
+displayPathtoPrincess _ grid    = let bot       = findCell grid 'm' 0
+                                      princess  = findCell grid 'p' 0 in
+                                      buildPath (fst bot - fst princess) ("UP\n", "DOWN\n")
+                                   ++ buildPath (snd bot - snd princess) ("LEFT\n", "RIGHT\n")
 
 -- Tail starts here
 main = do
-	n <- getLine
-    	let i = read n
-    	grid <- getList i
-    	putStrLn.displayPathtoPrincess i $ grid
+        n <- getLine
+        let i = read n
+        grid <- getList i
+        putStrLn.displayPathtoPrincess i $ grid
--- ai/introduction/saveprincess2.hs
+++ ai/introduction/saveprincess2.hs
@@ -15,31 +15,31 @@ getList n = if n==0 then return [] else do i <- getLine; is <- getList(n-1); ret
 select :: a -> [(Bool, a)] -> a
 select def = maybe def snd . find fst
 
-readPos				:: String -> (Int, Int)
-readPos xs			= let line = words xs in
-					(read (line !! 0), read (line !! 1))
+readPos                         :: String -> (Int, Int)
+readPos xs                      = let line = words xs in
+                                        (read (line !! 0), read (line !! 1))
 
-findCell			:: [String] -> Char -> Int -> (Int, Int)
-findCell (x:xs) cell acc	= case findIndex (== cell) x of
-  					Nothing -> findCell xs cell (1 + acc)
-					Just n  -> (acc, n)
+findCell                        :: [String] -> Char -> Int -> (Int, Int)
+findCell (x:xs) cell acc        = case findIndex (== cell) x of
+                                        Nothing -> findCell xs cell (1 + acc)
+                                        Just n  -> (acc, n)
 
-step				:: Int -> Int -> String
-step dy dx			| dx < 0	= "RIGHT\n"
-				| dx > 0	= "LEFT\n"
-				| dy < 0	= "DOWN\n"
-				| dy > 0	= "UP\n"
-				| otherwise	= ""
+step                            :: Int -> Int -> String
+step dy dx                      | dx < 0        = "RIGHT\n"
+                                | dx > 0        = "LEFT\n"
+                                | dy < 0        = "DOWN\n"
+                                | dy > 0        = "UP\n"
+                                | otherwise     = ""
 
-nextMove			:: Int -> Int -> Int -> [String] -> String
-nextMove _ r c grid		= let princess  = findCell grid 'p' 0 in
-				      step (r - fst princess) (c - snd princess)
+nextMove                        :: Int -> Int -> Int -> [String] -> String
+nextMove _ r c grid             = let princess  = findCell grid 'p' 0 in
+                                      step (r - fst princess) (c - snd princess)
 
 -- Tail starts here
 main = do
-	n <- getLine
-	m <- getLine
-    	let i = read n
-	let bot = readPos m
-    	grid <- getList i
-    	putStrLn((uncurry $ nextMove i) bot grid)
+        n <- getLine
+        m <- getLine
+        let i = read n
+        let bot = readPos m
+        grid <- getList i
+        putStrLn((uncurry $ nextMove i) bot grid)
