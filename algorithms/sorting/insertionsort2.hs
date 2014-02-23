644d19b8f75ba8963c239db6c484b0c415b04908
Ryan DV <rdevilla@uwaterloo.ca>
2014-02-23 17:20:42 -0500

    Add solution to Insertion Sort - Part 2

---
+++ algorithms/sorting/insertionsort2.hs
@@ -0,0 +1,29 @@
+-----------------------------------------------------------
+-- Solution to Algorithms/Sorting/Insertion Sort - Part 2
+-- https://www.hackerrank.com/challenges/insertionsort2
+-----------------------------------------------------------
+import Control.Monad.Writer
+
+tellArray                 :: [Int] -> Writer String [Int]
+tellArray xs              = (tell $ (unwords . map show $ xs) ++ "\n") >> return xs
+
+insertionSort             :: [Int] -> Writer String [Int]
+insertionSort (x:xs)      = insertionSort' xs [x] where
+
+  insertionSort'              :: [Int] -> [Int] -> Writer String [Int]
+  insertionSort' [] acc       = tell "" >> return acc
+  insertionSort' (x:xs) acc   = tellArray (insertionShift x acc ++ xs) >> insertionSort' xs (insertionShift x acc)
+
+  insertionShift            :: Int -> [Int] -> [Int]
+  insertionShift x []       = [x]
+  insertionShift x (y:ys)   | x <= y    = x:y:ys
+                            | otherwise = y:insertionShift x ys
+
+main :: IO ()
+main = do
+  getLine
+  elements <- getLine
+
+  let array = map read . words $ elements
+
+  putStrLn . snd . runWriter $ insertionSort array
